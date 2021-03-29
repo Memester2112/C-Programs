f1e6285583626cf8dbdcb1fcceaf02390f8bbdc0
2efb7c0586666e0570786fb9594a1329a46582b6 <aryendrasingh1525@gmail.com>
2021-03-29 16:27:54 +0530

    Testing CUDA and OPENMP Programs, some test files changed

---
+++ MonteCarlo.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+#include<math.h>
+#include<unistd.h>
+
+double dist(double x, double y)
+{
+	return sqrt(x*x + y*y);
+}
+
+int main()
+{
+	srand(time(NULL));//seeding
+	double pi = M_PI;
+	
+	double x ;
+	double y ;
+	printf("Number of processors available = %d\n\n\n",omp_get_num_procs());
+	
+	double my_pi = 0;
+	long long square = 0;
+	long long circle = 0;
+	double threshold = 1e-2;
+	double time_spent = 0.0;
+	
+	printf("Serial Code \n");
+	clock_t begin_1 = clock();
+	
+	while(abs(pi - my_pi) > threshold)
+	{
+		x = 1.0*(rand())/RAND_MAX;
+		y = 1.0*(rand())/RAND_MAX;
+		// printf("%f, %f \n",x,y);
+		square++;
+		if (dist(x,y)<=1.0)
+		{
+			circle++;
+		}
+		my_pi = 4*1.0*circle/square;
+		// printf("circle = %d, square = %d PI = %f\n",circle,square,my_pi);
+
+	}
+	
+	clock_t end_1 = clock();
+	time_spent += (double)(end_1 - begin_1) / CLOCKS_PER_SEC * 1000;
+	printf("Time elpased is %f milliseconds\n", time_spent);
+	
+	printf("%f\n\n\n",my_pi);
+	
+	sleep(1);
+	
+	printf("Parallel code\n");
+	
+	time_spent = 0;
+	square = 0;
+	circle = 0;
+	my_pi = 0.0;
+	int threadNums = 12;
+	clock_t begin_2 = clock();
+	int cir[threadNums];
+	for (int i = 0; i < threadNums; i++)
+	{
+		cir[i] = 0;
+	}
+	
+	// #pragma omp parallel
+	while(abs(pi-my_pi) > threshold)
+	{
+		#pragma omp for
+			for (int i = 0; i < threadNums; i++)
+			{
+				srand(time(0)*rand());
+				x = 1.0*(rand())/RAND_MAX;
+				y = 1.0*(rand())/RAND_MAX;
+				if(dist(x,y)<=1.0)
+					cir[i]++;
+				// printf("%f, %f TID = %d\n",x,y,i);
+			}
+		
+		// printf("\nOutside for loop in TID = %d\n",omp_get_thread_num());
+		square += threadNums;
+		circle = 0;
+		for (int i = 0; i < threadNums; i++)
+			circle += cir[i];
+		my_pi = 4*1.0*circle/square;
+		// printf("\nSleeping for 5 seconds circle = %d square = %d my_pi = %f\n",circle,square,my_pi);
+		// sleep(1);
+	}
+	
+	clock_t end_2 = clock();
+	time_spent += (double)(end_2 - begin_2) / CLOCKS_PER_SEC * 1000;
+	printf("Time elpased is %f milliseconds\n", time_spent);
+	
+	printf("%f\n",my_pi);
+	
+	return 0;
+}
--- Test1/main.c
+++ Test1/main.c
@@ -1,8 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+// int main()
+// {
+	// printf("Number of processors available = %d\n",omp_get_num_procs());
+	// double time_spent = 0.0;
+	// clock_t begin = clock();
+    // #pragma omp parallel
+    // {
+    	// printf("Hello world!\n");
+    // }
+	// clock_t end = clock();
+	// time_spent += (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+	// printf("Time elpased is %f milliseconds\n\n\n", time_spent);
+	
+	// time_spent = 0.0;
+	// clock_t begin1 = clock();
+    // for(int i = 0;i < 128; i ++)
+	// {
+		// printf("Hello world!\n");
+	// }
+	// clock_t end1 = clock();
+	// time_spent += (double)(end1 - begin1) / CLOCKS_PER_SEC * 1000;
+	// printf("Time elpased is %f milliseconds\n\n\n", time_spent);
+	
+    
+    // return 0;
+// }
+
+// int main(int argc, char* argv[])
+// {
+	// int id;
+	// double wtime;
+	// printf("Number of processors available = %d\n",omp_get_num_procs());
+	// printf("Number of threads = %d\n",omp_get_max_threads());
+	// wtime = omp_get_wtime();
+	// printf("OUTSIDE Parallel region\n");
+	
+	// id = omp_get_thread_num();
+	// printf("Hello from process %d\n Going into parallel zone \n",id);
+	
+	// #pragma omp parallel private(id) 
+	// {
+		// id = omp_get_thread_num();
+		// printf("Hello from process number %d\n",id);
+	// }
+	
+	// wtime = omp_get_wtime()-wtime;
+	
+	// printf("Back outside the parallel region\nTime elapsed = %f\n",wtime);
+	// return 0;
+// }
+
+bool isPrime(int n)
+{
+	if (n == 2 || n == 3 || n == 5 || n == 7)
+		return 1;
+	if ( n%2 == 0 || n%3 == 0 || n%5 == 0 || n%7 == 0)
+		return 0;
+	int count = 0;
+	for (int i = 2; i < n/2 + 1; i ++)
+	{
+		if (n % i == 0)
+			count ++;
+	}
+	if (count > 0)
+		return 0;
+	else
+		return 1;
+}
+
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+	printf("Number of processors available = %d\n",omp_get_num_procs());
+	int num = 0;
+	int N = 5000;//131072
+		
+	printf("Parallel code\n");
+	int j = 2;
+	double time_spent = 0.0;
+	clock_t begin_1 = clock();
+	num = 0;
+	#pragma omp parallel
+	#pragma omp for
+		for(int i = 2;i < N; i ++)
+		{
+			if (isPrime(i))
+			{
+				// sleep(0.1);
+				#pragma omp atomic
+				num++;
+				// printf("%d ",i);
+			}
+		}
+	printf("\n%d\n",num);
+	clock_t end_1 = clock();
+	time_spent += (double)(end_1 - begin_1) / CLOCKS_PER_SEC * 1000;
+	printf("Time elpased is %f milliseconds\n\n\n", time_spent);
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	printf("Serial Code\n");
+	num = 0;
+	time_spent = 0.0;
+	clock_t begin = clock();
+	for (int i = 2;i <= N; i++) 
+	{
+		if (isPrime(i))
+		{
+			num++;
+			// printf("%d ",i);
+		}
+	}
+	printf("\n%d\n",num);
+	clock_t end = clock();
+	time_spent += (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+	printf("Time elpased is %f milliseconds\n\n\n", time_spent);
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	return 0;
 }
